4aafbb28acc0740672a9c516faae257cc3807659
htthtt12t1 <61179290+htthtt12t1@users.noreply.github.com>
2020-02-29 14:15:31 +0700

    Update lca_hld.cpp

--- Graph/lca_hld.cpp
+++ Graph/lca_hld.cpp
@@ -1,75 +1,83 @@
 #include<bits/stdc++.h>
  
 using namespace std;
-
-const int maxn = 2e5 + 5;
-int n, q, id;
+const int maxn = 3e5 + 4;
 typedef pair<int,int> ii;
-vector<int>a[maxn];
-int nChain, Chain[maxn], ChainHead[maxn], ansChain, h[maxn], nChild[maxn], par[maxn], first[maxn][2];
-int mark[maxn];
-ii pass[maxn];
+vector<ii> a[maxn];
+int n, m, id, nChain, nChild[maxn], ChainHead[maxn], Chain[maxn], par[maxn], X[maxn], nPos;
+int ts[maxn*4], h[maxn], pos[maxn], st[maxn*4];
 
-void DFS(int u, int height = 0, int p = -1)
+typedef pair<int,int> ii;
+void _accepted_by_one_hit_()
 {
-    nChild[u] = 1;
+    #ifdef NTMA
+        freopen("abc.inp", "r", stdin);
+        freopen("abc.out", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+void DFS(int u, int height)
+{
     h[u] = height;
+    nChild[u] = 1;
     for(auto v: a[u]){
-        if (v == p) continue;
-        DFS(v, height + 1, u);
-        par[v] = u;
-        nChild[u] += nChild[v];
+        if (v.first == par[u]) continue;
+        par[v.first] = u;
+        DFS(v.first, height+1);
+        nChild[u] += nChild[v.first];
     }
 }
-void hld(int u)
+
+void hld(int u, int val = -1)
 {
     if (ChainHead[nChain] == 0) ChainHead[nChain] = u;
     Chain[u] = nChain;
-    
-    int next = -1;
-    for(auto v: a[u]) if (v != par[u]){
-        if (next == -1 || nChild[next] < nChild[v]) next = v;
-    }
-    if (next != -1) hld(next);
-    for(auto v: a[u]) if (v != next && v != par[u]) {
-        ++nChain;
-        hld(v);
-    }
-}
-void SFD(int u, int k)
-{   
-    int type = (k < 0);
-    first[Chain[u]][type] = u;
-    if (!type) pass[Chain[u]] = ii(k, id);
-    else if (pass[Chain[u]].first == -k && pass[Chain[u]].second == id) {
-        ansChain = Chain[u];
-        return;
-    }
-    if (Chain[u] == 1) return;
-    int v = par[ChainHead[Chain[u]]];
-    SFD(v, k);
-}
-int lca(int u, int v)
-{
-    SFD(u, v); SFD(v, -v);
-    int x = first[ansChain][0], y = first[ansChain][1];
-    if (h[x] > h[y]) return y; else return x;
-}
+    pos[u] = ++nPos;
+    X[nPos] = val;
 
+    int nxt = -1, valnxt = -1;
+    for(auto v: a[u]) if (v.first != par[u]){
+        if (nChild[v.first] > nChild[nxt] || nxt == -1) {
+            nxt = v.first;
+            valnxt = v.second;
+        }
+    }
+    if (nxt != -1) hld(nxt, valnxt);
+    for(auto v: a[u]){
+        if (v.first != nxt && v.first != par[u]) {
+            ++nChain;
+            hld(v.first, v.second);
+        }
+    }
+}
+int get_ans(int u, int v)
+{
+    while (Chain[u] != Chain[v]) {
+        int hu = ChainHead[Chain[u]], hv = ChainHead[Chain[v]];
+        if (h[hu] < h[hv]) swap(u, v), swap(hu, hv);
+        u = par[hu]; 
+    }
+    if (h[u] < h[v]) return u; else return v;
+    return res;
+}
 int main()
 {
-    cin >> n >> q;
-    for(int i=1; i<n; ++i) {            
-        int u, v;
-        cin >> u >> v;
-        a[u].push_back(v);
-        a[v].push_back(u);
-    }
-    DFS(1);
-    nChain = 1; hld(1);
+    _accepted_by_one_hit_();
 
-    for(id =1; id<=q; ++id){
-        int u, v; cin >> u >> v;
-        cout << lca(u, v) << "\n";
+    int test; test = 1;
+    while (test--){
+        cin >> n;
+        for(int i=1; i<n; ++i) {
+            int u, v, c; cin >> u >> v >> c;
+            a[u].push_back(ii(v, c));
+            a[v].push_back(ii(u, c));
+        }
+        DFS(1, 0); nChain = 1; hld(1);
+        int q; cin >> q;
+        while (q--){
+            int u, v; cin >> u >> v;
+            cout << get_ans(u, v);
+        }
     }
 }
